43d12378f5c2c278a71169f3f8f587157c130c49
gatonymous <124922467+gatonymous@users.noreply.github.com>
2023-12-28 09:40:30 -0500

    start

---
+++ app/Http/Controllers/PortafolioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePortafolioRequest;
+use App\Http\Requests\UpdatePortafolioRequest;
+use App\Models\Portafolio;
+
+class PortafolioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePortafolioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Portafolio $portafolio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Portafolio $portafolio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePortafolioRequest $request, Portafolio $portafolio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Portafolio $portafolio)
+    {
+        //
+    }
+}
